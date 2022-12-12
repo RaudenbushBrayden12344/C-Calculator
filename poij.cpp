c4da5499b8975d4cc770a463daf923c28b30e28b
EC2 Default User <ec2-user@ip-172-31-83-47.ec2.internal>
2022-12-12 18:45:01 +0000

    Worked on calculator basics

--- poij.cpp
+++ poij.cpp
@@ -1,49 +1,49 @@
-// C++ program to create calculator using
-// switch statement
-#include <iostream>
+#include<iostream>
 using namespace std;
- 
-// Main program
 int main()
 {
-    char op;
-    float num1, num2;
- 
-    // It allows user to enter operator i.e. +, -, *, /
-    cin >> op;
- 
-    // It allow user to enter the operands
-    cin >> num1 >> num2;
- 
-    // Switch statement begins
-    switch (op) {
-         
-        // If user enter +
-        case '+':
-            cout << num1 + num2;
-            break;
-         
-        // If user enter -
-        case '-':
-            cout << num1 - num2;
-            break;
-         
-        // If user enter *
-        case '*':
-            cout << num1 * num2;
-            break;
-         
-        // If user enter /
-        case '/':
-            cout << num1 / num2;
-            break;
-         
-        // If the operator is other than +, -, * or /,
-        // error message will display
-        default:
-            cout << "Error! operator is not correct";
-            break;
-    } // switch statement ends
- 
+    float numOne, numTwo, res;
+    int choice;
+    do
+    {
+        cout<<"1. Addition\n";
+        cout<<"2. Subtraction\n";
+        cout<<"3. Multiplication\n";
+        cout<<"4. Division\n";
+        cout<<"5. Exit\n\n";
+        cout<<"Enter Your Choice(1-5): ";
+        cin>>choice;
+        if(choice>=1 && choice<=4)
+        {
+            cout<<"\nEnter any two Numbers: ";
+            cin>>numOne>>numTwo;
+        }
+        switch(choice)
+        {
+            case 1:
+                res = numOne+numTwo;
+                cout<<"\nResult = "<<res;
+                break;
+            case 2:
+                res = numOne-numTwo;
+                cout<<"\nResult = "<<res;
+                break;
+            case 3:
+                res = numOne*numTwo;
+                cout<<"\nResult = "<<res;
+                break;
+            case 4:
+                res = numOne/numTwo;
+                cout<<"\nResult = "<<res;
+                break;
+            case 5:
+                return 0;
+            default:
+                cout<<"\nWrong Choice!";
+                break;
+        }
+        cout<<"\n------------------------\n";
+    }while(choice!=5);
+    cout<<endl;
     return 0;
 }
